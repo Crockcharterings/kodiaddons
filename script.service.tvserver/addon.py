a881fa22825222da0d010ae427d05fbfda9f1773
roman <r.leschuk@gmail.com>
2018-01-06 02:36:48 +0300

    first commit

---
+++ script.service.tvserver/addon.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import xbmcaddon
+import xbmcgui
+import sys
+import os
+
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
+
+sys.path.insert(0, addon.getAddonInfo('path')+'/resources/lib/')
+
+from flask import (Flask, render_template, Response, redirect, url_for, request, jsonify, abort)
+
+version = ".".join(map(str, sys.version_info[:3]))
+
+xbmcgui.Dialog().ok(addonname, addon.getAddonInfo('path'), __name__, version)
+
+app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, threaded=True, port=8081)
