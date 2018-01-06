0884282109f7d25039a03cf03be49693e8ef6f86
roman <r.leschuk@gmail.com>
2018-01-06 13:28:08 +0300

    06012018

--- script.service.tvserver/addon.py
+++ script.service.tvserver/addon.py
@@ -1,20 +1,118 @@
 # -*- coding: utf-8 -*-
 
-import xbmcaddon
-import xbmcgui
-import sys
-import os
+import xbmc, xbmcaddon, xbmcgui
+import os, sys, json, logging
 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
-
+addon = xbmcaddon.Addon()
+addonname = addon.getAddonInfo('name')
 sys.path.insert(0, addon.getAddonInfo('path')+'/resources/lib/')
 
 from flask import (Flask, render_template, Response, redirect, url_for, request, jsonify, abort)
+from channels import Channels
 
 #version = ".".join(map(str, sys.version_info[:3]))
 #xbmcgui.Dialog().ok(addonname, addon.getAddonInfo('path'), __name__, version)
 
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
+chls = Channels()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/api/groups/get')
+def api_groups_get():
+    groups = chls.get_groups()
+    return jsonify(groups=dict(get=groups))
+
+@app.route('/api/groups/disable')
+def api_groups_disable():
+    name   = request.args.get('name')
+    status = jsonloads(request.args.get('status'))
+    if name and isinstance(status, bool):
+        chls.group_toggle(name, status)
+        return jsonify(groups=dict(disable=dict(
+            name=name, status=status)))
+    return abort(400)
+
+@app.route('/api/channels/get')
+def api_channels_get():
+    group    = request.args.get('group')
+    channels = chls.get_channels(disabled=True, group=group)
+    return jsonify(channels=dict(get=channels))
+
+@app.route('/api/channels/links')
+def api_channels_links():
+    name = request.args.get('name')
+    if name:
+        channels = chls.get_channels(name)
+        return jsonify(channels=dict(links=channels))
+    return abort(400)
+
+@app.route('/api/channels/disable')
+def api_channels_disable():
+    name   = request.args.get('name')
+    status = jsonloads(request.args.get('status'))
+    if name and isinstance(status, bool):
+        chls.channel_toggle(name, status)
+        return jsonify(channels=dict(disable=dict(
+            name=name, status=status)))
+    return abort(400)
+
+@app.route('/api/channels/disable_resource')
+def api_channels_disable_resource():
+    hash_  = request.args.get('hash')
+    status = jsonloads(request.args.get('status'))
+    if hash_ and isinstance(status, bool):
+        chls.link_toggle(hash_, status)
+        return jsonify(channels=dict(disable_resourse=dict(
+            hash=hash_, status=status)))
+    return abort(400)
+
+@app.route('/api/channels/update')
+def api_channels_update():
+    ch_name = request.args.get('ch_name')
+    gr_name = request.args.get('gr_name')
+    channel_title = request.args.get('channel_title')
+    group_title = request.args.get('group_title')
+    if len(channel_title) > 0:
+        chls.update_channel(ch_name, gr_name, channel_title, group_title)
+        return jsonify(channels=dict(update=dict(
+            ch_name=ch_name, gr_name=gr_name,
+            channel_title=channel_title, group_title=group_title)))
+    return abort(400)
+
+@app.route('/playlist')
+def playlist():
+    ace_host = request.args.get('acestream_host',ACESTREAM_HOST)
+    ace_port = request.args.get('acestream_port',ACESTREAM_PORT)
+    hd_priority = request.args.get('hd_priority',0)
+    p2p_priority = request.args.get('p2p_priority',0)
+    m3u = chls.get_playlist(
+        hd_priority=hd_priority,
+        p2p_priority=p2p_priority,
+        acestream_host=ace_host,
+        acestream_port=ace_port
+    )
+    return Response(m3u, status=200, mimetype='text/plain')
+
+@app.route('/channel')
+def channel():
+    name = request.args.get('name')
+    ace_host = request.args.get('acestream_host',ACESTREAM_HOST)
+    ace_port = request.args.get('acestream_port',ACESTREAM_PORT)
+    hd_priority = request.args.get('hd_priority',0)
+    p2p_priority = request.args.get('p2p_priority',1)
+    channel_link = chls.get_channel(name,
+        hd_priority=hd_priority,
+        p2p_priority=p2p_priority,
+        acestream_host=ace_host,
+        acestream_port=ace_port
+    )
+    if channel_link: return redirect(channel_link)
+    return abort(403)
+
 if __name__ == '__main__':
+    xbmc.executebuiltin('Notification(%s, %s, %d, %s)' % (addonname, 'TVServer started', 5000, ''))
     app.run(host='0.0.0.0', debug=False, threaded=True, port=8081)
---
+++ script.service.tvserver/resources/lib/channels.py
@@ -0,0 +1,285 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os, logging, json, codecs, urllib2, requests, ssl, imp, importlib, time, subprocess, re
+#from threading import Thread
+from datetime import datetime, timedelta
+#import pandas as pd
+import sqlite3
+from config import *
+logger = logging.getLogger(__name__)
+
+class Channels(object):
+    def __init__(self):
+        object.__init__(self)
+        self.mychannels = dict()
+        self.host = TVSERVER_HOST
+        self.port = TVSERVER_PORT
+        self.ace_process = None
+
+    def _request(self, url):
+        try:
+            request = urllib2.Request(url)
+            response = urllib2.urlopen(request, context=ssl._create_unverified_context())
+            data = response.read()
+            return data
+        except: pass
+
+    def update_channels(self):
+        conn = sqlite3.connect(GLOBAL_PATH + '/database.db')
+        file, pathname, description = imp.find_module('modules')
+        if file:
+            logger.critical('Not a package: modules')
+            return
+        for module in os.listdir(pathname):
+            if module.endswith('.py') and not module.startswith('__') and 'fanattv' in module:
+                try:
+                    logger.info('update channels from module %s', module)
+                    mod = importlib.import_module('modules.'+module[:-3])
+                    resource = mod.TVResource()
+                    channels = resource.get_channels()
+                    resource_hash = abs(hash(resource.urlparse.netloc))
+                    channels_db = pd.read_sql_query("""
+                        select * from channels_orig
+                        where resource_hash = {0}""".format(resource_hash), conn)
+                    for channel in channels:
+                        item = channels_db.loc[channels_db['hash'] == channel['hash']]
+                        if not item.empty:
+                            item = item.to_dict(orient='record')[0]
+                            if item['resource'] != module:
+                                logger.info('update channel %s resource', channel['title'])
+                                conn.cursor().execute("""
+                                    update channels_orig set resource = ?, timestamp = ?
+                                    where hash = ? and resource_hash = ?""",
+                                    (module, int(time.time()-time.mktime(time.gmtime(0))), channel['hash'], resource_hash))
+                                conn.commit()
+                            if item['link'] != channel['link']:
+                                logger.info('update channel %s link %s', channel['title'], channel['link'])
+                                conn.cursor().execute("""
+                                    update channels_orig set link = ?, timestamp = ?
+                                    where hash = ? and resource_hash = ?""",
+                                    (channel['link'], int(time.time()-time.mktime(time.gmtime(0))), channel['hash'], resource_hash))
+                                conn.commit()
+                            if item['logo'] != channel['logo']:
+                                logger.info('update channel %s logo %s', channel['title'], channel['logo'])
+                                conn.cursor().execute("""
+                                    update channels_orig set logo = ?, timestamp = ?
+                                    where hash = ? and resource_hash = ?""",
+                                    (channel['logo'], int(time.time()-time.mktime(time.gmtime(0))), channel['hash'], resource_hash))
+                                conn.commit()
+                            if item['cost'] != channel['cost']:
+                                logger.info('update channel %s cost %s', channel['title'], channel['cost'])
+                                conn.cursor().execute("""
+                                    update channels_orig set cost = ?, timestamp = ?
+                                    where hash = ? and resource_hash = ?""",
+                                    (channel['cost'], int(time.time()-time.mktime(time.gmtime(0))), channel['hash'], resource_hash))
+                                conn.commit()
+                        else:
+                            logger.info('add channel %s', channel['title'])
+                            conn.cursor().execute("""
+                                insert into channels_orig (
+                                resource, resource_hash, hash, title, logo, link, group_, cost, timestamp)
+                                values (?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+                                (module, resource_hash, channel['hash'], channel['title'], channel['logo'],
+                                 channel['link'], channel['group'], channel['cost'],
+                                 int(time.time()-time.mktime(time.gmtime(0)))))
+                            conn.commit()
+                except Exception as e:
+                    logger.error('%s: %s', module, repr(e))
+        conn.close()
+
+    def update_channel(self, ch_name, gr_name, channel_title, group_title):
+        conn = sqlite3.connect(GLOBAL_PATH + '/database.db')
+        try:
+        	conn.cursor().execute("""
+        		update channels_names set name = ?
+        		where name = ?""",
+        		(channel_title, ch_name))
+        	conn.commit()
+        	conn.cursor().execute("""
+        		update channels_groups set group_title = ?
+        		where group_title = ? and channel_hash in (
+        		select channel_hash from channels_names where name = ?)""",
+        		(group_title, gr_name, channel_title))
+        	conn.commit()
+        except Exception as e:
+            logger.error(repr(e))
+        conn.close()
+
+    def get_groups(self):
+        conn = sqlite3.connect(GLOBAL_PATH + '/database.db')
+        groups = pd.read_sql_query("""
+            select (select count(*) from channels_groups) as cnt,
+            -1 as disabled, 'ВСЕ КАНАЛЫ' as group_title
+            union all
+            select * from (
+            select count(cg.channel_hash) as cnt, g.disabled,
+            g.title as group_title
+            from groups g
+            left join channels_groups cg on g.title = cg.group_title
+            group by g.title order by g.title)
+            """, conn).fillna('').to_dict(orient='record')
+        return groups
+        conn.close()
+
+    def group_toggle(self, name, status):
+        conn = sqlite3.connect(GLOBAL_PATH + '/database.db')
+        try:
+            conn.cursor().execute("""
+                update groups set disabled = ?
+                where title = ?""",
+                (int(not status), name))
+            conn.commit()
+        except: pass
+        conn.close()
+
+    def link_toggle(self, hash_, status):
+        conn = sqlite3.connect(GLOBAL_PATH + '/database.db')
+        try:
+            conn.cursor().execute("""
+                update channels_orig set disabled = ?
+                where hash = ?""",
+                (int(not status), hash_))
+            conn.commit()
+        except Exception as e: logger.error(repr(e))
+        conn.close()
+
+    def get_channels(self, name=None, disabled=False, group=None):
+        conn = sqlite3.connect(GLOBAL_PATH + '/database.db')
+        channels = dict()
+        if name:
+            channels_db = pd.read_sql_query("""
+                select co.*, cn.name from channels_orig co
+                inner join channels_names cn on co.hash = cn.channel_hash
+                inner join resources r on co.resource = r.resource and r.disabled = 0
+                where cn.name = '{0}'
+                order by cn.name, cost
+                """.format(name.encode('utf8').replace("'","''")), conn).fillna('')
+        elif disabled:
+            group_filter = ""
+            if group: group_filter = "where group_title = '{}'".format(group.encode('utf8'))
+            channels_db = pd.read_sql_query("""
+                select * from channels {}
+                """.format(group_filter), conn).fillna('')
+        else:
+            channels_db = pd.read_sql_query("""
+                select * from channels
+                where gr_disabled = 0
+                and ch_disabled <> cnt
+                """, conn).fillna('')
+        channels = channels_db.to_dict(orient='record')
+        conn.close()
+        return channels
+
+    def get_channel(self, name, hd_priority=0, p2p_priority=0,
+                          acestream_host=ACESTREAM_HOST, acestream_port=ACESTREAM_PORT):
+        channels = self.get_channels(name)
+        if channels:
+            channels = self.order_channels(channels, hd_priority=hd_priority, p2p_priority=p2p_priority)
+            for channel in channels:
+                if channel['disabled'] == 1: continue
+                module = 'modules.'+channel['resource'][:-3]
+                url = importlib.import_module(module).TVResource().get_stream(channel)
+                if url:
+                    if url.startswith('/ace/'):
+                        url = ''.join(['http://', acestream_host, ':', acestream_port, url])
+                    logger.info('get_stream: %s', url)
+                    test = self.test_channel(url, acestream_host, acestream_port)
+                    logger.info('test %s: %s', url, test)
+                    if test < 999: return url
+            logger.error('%s: no working streams', name)
+
+    def channel_toggle(self, name, status):
+        conn = sqlite3.connect(GLOBAL_PATH + '/database.db')
+        try:
+            conn.cursor().execute("""
+                update channels_orig set disabled = ?, timestamp = ?
+                where hash in (select channel_hash from channels_names where name = ?)""",
+                (int(not status), int(time.time()-time.mktime(time.gmtime(0))), name))
+            conn.commit()
+        except: pass
+        conn.close()
+
+    def get_playlist(self, hd_priority=0, p2p_priority=0,
+                           acestream_host=ACESTREAM_HOST, acestream_port=ACESTREAM_PORT):
+        m3u = []
+        m3u.append('#EXTM3U')
+        for channel in self.get_channels():
+            logo = self.get_logo(channel)
+            m3u.append(''.join([
+                '#EXTINF:-1 group-title="', channel.get('group_title'),
+                '" tvg-name="', channel.get('name'),
+                '" tvg-id="" tvg-logo="', logo,
+                '",', channel.get('name')
+            ]))
+            url = [
+                'http://{host}:{port}/channel?name={name}&hd={hd}&p2p={p2p}'.format(
+                    host=self.host,
+                    port=self.port,
+                    name=urllib2.quote(channel.get('name').encode('utf8')),
+                    hd=hd_priority,
+                    p2p=p2p_priority
+                )]
+            if acestream_host != ACESTREAM_HOST: url.append('acestream_host='+acestream_host)
+            if acestream_port != ACESTREAM_PORT: url.append('acestream_port='+acestream_port)
+            m3u.append('&'.join(url))
+        return '\n'.join(m3u)
+
+    def order_channels(self, channels, hd_priority=0, p2p_priority=0):
+        tmp = channels
+        result = []
+        if hd_priority:
+            result.extend([c for c in tmp if re.search('HD', c['title'])])
+            result.extend([c for c in tmp if not re.search('HD', c['title'])])
+        else:
+            result.extend([c for c in tmp if not re.search('HD', c['title'])])
+            result.extend([c for c in tmp if re.search('HD', c['title'])])
+        tmp = result
+        result = []
+        if p2p_priority:
+            result.extend([c for c in tmp if re.search('(^/ace/|acestream)', c['link'])])
+            result.extend([c for c in tmp if not re.search('(^/ace/|acestream)', c['link'])])
+        else:
+            result.extend([c for c in tmp if not re.search('(^/ace/|acestream)', c['link'])])
+            result.extend([c for c in tmp if re.search('(^/ace/|acestream)', c['link'])])
+        return result
+
+    def test_channel(self, url, acestream_host, acestream_port):
+        try:
+            s = requests.session()
+            d = datetime.now()
+            if '/ace/' in url:
+                url = url.replace('ace/getstream', 'ace/manifest.m3u8')
+                link = ''.join([
+                    'http://', acestream_host, ':', acestream_port,
+                    '/webui/api/service?method=get_version&format=jsonp&callback=mycallback'
+                ])
+                result = self._request(link)
+                if result:
+                    return 1.
+                else:
+                    return 999.
+            else:
+                try:
+                    r = s.get(url, timeout=(1, 0.00001))
+                except requests.exceptions.ReadTimeout:
+                    delta = datetime.now()-d
+                    return float(delta.microseconds)/1000
+                except requests.exceptions.ConnectTimeout:
+                    logger.error('error test channel %s: ConnectTimeout', url)
+                    return 999.
+        except Exception as e:
+            logger.error('error test channel %s: %s', url, repr(e))
+            return 999.
+
+    def get_logo(self, channel):
+        try:
+            m = [f.decode('utf8') for f in os.listdir(GLOBAL_PATH + '/static/images/logos/')]
+            f = m[m.index(channel['name']+'.png')]
+            return 'http://{host}:{port}/static/images/logos/{name}'.format(
+                host=self.host,
+                port=self.port,
+                name=urllib2.quote(f)
+            )
+        except:
+            if channel['logo']: return channel['logo']
+            else: return ''
